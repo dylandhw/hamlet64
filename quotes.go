d931c143a2007a0e73e4cfe084992a238fc75316
dylandhw <dylanwilkins2308@gmail.com>
2026-02-20 00:10:24 -0500

    add some quotes

--- quotes.go
+++ quotes.go
@@ -2,4 +2,13 @@ package main
 
 // this does not need to be its own file but i dont like looking at long walls of quote text
 // going to spend a bit of time finding fav quotes
-var quotes = []string{}
+var quotes = []string{
+	"I do bite my thumb, sir",
+	"Would thou wert clean enough to spit upon!",
+	"I do desire we may be strangers",
+	"Thou whoreson zed, thou unnecessary letter!",
+	"You Banbury cheese!",
+	"I am sick when I do look on thee",
+	"I do desire we may be better strangers",
+	"There’s no more faith in thee than in a stewed prune",
+}
